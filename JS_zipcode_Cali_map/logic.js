f8aadb040f1da9216c0383afa531785efa21028b
Armann <115322974+armannphd@users.noreply.github.com>
2023-04-12 15:13:01 -0700

    updated Armann's code to add more data and border

--- JS_zipcode_Cali_map/logic.js
+++ JS_zipcode_Cali_map/logic.js
@@ -1,5 +1,9 @@
+// Function to add commas as thousands separators
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 // Load the JSON data using d3.json()
-d3.json('table2.json')
+d3.csv('median_prices.csv')
   .then(data => {
     // Define the layers
     var lowPriceLayer = L.layerGroup();
@@ -10,11 +14,19 @@ d3.json('table2.json')
     // Loop through the data and add the markers to the appropriate layer
     //create the lowPriceLayer 
     for (var i = 0; i < data.length; i++) {
-      var zipCode = data[i].zip_code;
-      var medianPrice = data[i].Median;
+      var zipCode = data[i].zipcode;
+      var medianPrice = data[i].median;
       var mortgage = data[i].mortgage;
-      var lat = data[i].lat;
-      var lng = data[i].lng;
+      var lat = data[i].lat_x;
+      var lng = data[i].lng_x;
+      var county = data[i].county;
+      var county_seat = data[i].county_seat;
+      var density = data[i].density;
+      var population = data[i].population;
+      var area = data[i].area;
+      var gdp = data[i].gdp_usd;
+      var gdp_rank = data[i].gdp_rank;
+      var city = data[i].city
 
       var circle;
       if (medianPrice < 850000) {
@@ -25,8 +37,10 @@ d3.json('table2.json')
           weight: 1,
           opacity: 1,
           fillOpacity: 0.7
-        }).bindPopup("Zip Code: " + zipCode + "<br>Median Price: $" + medianPrice);
+        }).bindPopup("Zip Code: " + zipCode + "<br>City: " + city + "<br><strong><span style='font-size: 1.2em;'>Median Price: $" + numberWithCommas(medianPrice) + "</span></strong><hr>Population (zipcode): " + numberWithCommas(population) + "<br>Density (zipcode): " + density + " persons per sq. mile" + "<hr>County: " + county + "<br>County Seat: " + county_seat + "<hr>Area (county): " + numberWithCommas(area) + "<hr>GDP (county): $" + numberWithCommas(gdp) + "<br>GDP Rank: " + gdp_rank + " of 58");
 
+
+       
         //add the lowPriceLayer
         lowPriceLayer.addLayer(circle);
       }
@@ -39,7 +53,7 @@ d3.json('table2.json')
           weight: 1,
           opacity: 1,
           fillOpacity: 0.8
-        }).bindPopup("Zip Code: " + zipCode + "<br>Median Price: $" + medianPrice);
+        }).bindPopup("Zip Code: " + zipCode + "<br>City: " + city + "<br><strong><span style='font-size: 1.2em;'>Median Price: $" + numberWithCommas(medianPrice) + "</span></strong><hr>Population (zipcode): " + numberWithCommas(population) + "<br>Density (zipcode): " + density + " persons per sq. mile" + "<hr>County: " + county + "<br>County Seat: " + county_seat + "<hr>Area (county): " + numberWithCommas(area) + "<hr>GDP (county): $" + numberWithCommas(gdp) + "<br>GDP Rank: " + gdp_rank + " of 58");
 
         //add the mediumPriceLayer
         mediumPriceLayer.addLayer(circle);
@@ -53,7 +67,7 @@ d3.json('table2.json')
           weight: 1,
           opacity: 1,
           fillOpacity: 0.8
-        }).bindPopup("Zip Code: " + zipCode + "<br>Median Price: $" + medianPrice);
+        }).bindPopup("Zip Code: " + zipCode + "<br>City: " + city + "<br><strong><span style='font-size: 1.2em;'>Median Price: $" + numberWithCommas(medianPrice) + "</span></strong><hr>Population (zipcode): " + numberWithCommas(population) + "<br>Density (zipcode): " + density + " persons per sq. mile" + "<hr>County: " + county + "<br>County Seat: " + county_seat + "<hr>Area (county): " + numberWithCommas(area) + "<hr>GDP (county): $" + numberWithCommas(gdp) + "<br>GDP Rank: " + gdp_rank + " of 58");
 
         //add the highPriceLayer
         highPriceLayer.addLayer(circle);
@@ -64,10 +78,18 @@ d3.json('table2.json')
     }
 
     // Define the base layers
-    var baseLayers = {
-      "StreetMap": L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    var streetLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      })
+      });
+    
+    var topoLayer = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://opentopomap.org/about">OpenTopoMap</a> contributors'
+      });
+
+    // Define the base layers
+    let baseLayers = {
+      "StreetMap": streetLayer,
+      "Topography": topoLayer
     };
 
     // Define the overlay layers
@@ -88,8 +110,29 @@ d3.json('table2.json')
       center: [37.25, -119.75],
       zoom: 6,
       layers: [baseLayers.StreetMap]
-});
+    });
 
-// Add layer control
-L.control.layers(baseLayers, overlayLayers).addTo(map);
-});
+    // Add layer control
+    L.control.layers(baseLayers, overlayLayers).addTo(map);
+    
+    // Add a definitive border for the State of California
+    function style(feature) {
+      return {
+        color: 'orangered',
+        weight: 5,
+        opacity: 1,
+        fillOpacity: 0
+      };
+    }
+
+    // Load the JSON data using d3.json()
+    d3.json('gz_2010_us_040_00_500k.json')
+      .then(statesData => {
+        L.geoJSON(statesData, {
+          filter: feature => feature.properties.NAME == 'California',
+          style: style
+        }).addTo(map);
+      });
+  });
+
+
