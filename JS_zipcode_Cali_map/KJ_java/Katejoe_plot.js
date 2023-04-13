1258c3b6a01990b2ce804306beffa76ccc4cb98e
kaakabogu <119997774+kaakabogu@users.noreply.github.com>
2023-04-13 11:45:48 -0500

    Update the demographics visual

--- JS_zipcode_Cali_map/KJ_java/Katejoe_plot.js
+++ JS_zipcode_Cali_map/KJ_java/Katejoe_plot.js
@@ -4,7 +4,6 @@ d3.json("dem.json").then(function(data) {
 });
 function generateData(data, selectedField) {
     var Zipcode= []
-    //var County = []
     var Incomepercapita = []
     var Medianhouseholdincome = []
     var Poverty = []
@@ -15,7 +14,6 @@ function generateData(data, selectedField) {
 
     for (let i = 0; i < data.length; i++) {
         Zipcode.push(data[i].Zipcode);
-        //County.push(data[i].County);
         Incomepercapita.push(data[i].Income_per_capita);
         Medianhouseholdincome.push(data[i].Median_household_income);
         Poverty.push(data[i].Poverty);
@@ -25,32 +23,46 @@ function generateData(data, selectedField) {
         Transitpublictransport.push(data[i].Transit_public_transport);
         
     }
-    buildBarChart(Zipcode, selectedField, //County, 
-    Incomepercapita, Medianhouseholdincome, Poverty, Medianage, Medianhomevalue, TotalPopulation, Transitpublictransport);
-    //buildBubbleChart(medianHomePrice, passingStudents, numberStudents);
+    buildLineChart(Zipcode, selectedField, Incomepercapita, Medianhouseholdincome, Poverty, Medianage, Medianhomevalue, TotalPopulation, Transitpublictransport);
+    
 }
-function buildBarChart(Zipcode, selectedField, //County, 
-Incomepercapita, Medianhouseholdincome, Poverty, Medianage, Medianhomevalue, TotalPopulation, Transitpublictransport) {
-    let trace1 = {
+function buildLineChart(Zipcode, selectedField, Incomepercapita, Medianhouseholdincome, Poverty, Medianage, Medianhomevalue, TotalPopulation, Transitpublictransport) {
+    
+    var trace = {
         x: Zipcode,
         y: eval(selectedField),
-        type: "bar",
-        mode: "group",
-        //text: County
-    };
-    let layout = {
+        type: 'bar',
+        mode: 'markers',
+        text: Zipcode,
+        marker: {
+          color: 'Indigo'
+        }
+      };
+      
+      // Define layout object
+      var layout = {
+        title: 'Demographics',
         xaxis: {
-            title: "Zipcode"
+          title: 'Zipcode',
+          type: 'category'
         },
         yaxis: {
-            title: selectedField
+          title: selectedField
         }
-        };
-        let traceData = [trace1];
-        Plotly.newPlot("bar", traceData, layout);
+      };
+      
+      // Create array of trace objects
+      var data = [trace];
+      
+      // Create line chart
+      Plotly.newPlot('chart', data, layout);
+ 
 }
 function optionChanged(selectedField) {
     d3.json("dem.json").then(function(data) {
         generateData(data, selectedField);
     });
 };
+
+
+
